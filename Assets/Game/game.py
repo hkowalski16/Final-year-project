2cb7a36ac1b7affa712edcc188ac66359edd90f0
hkowalski16 <90383181+hkowalski16@users.noreply.github.com>
2023-03-06 12:56:25 +0000

    Found the coordinates for the checkpoints
    Next will be to get the enemies moving on the path

--- Assets/Game/game.py
+++ Assets/Game/game.py
@@ -1,11 +1,20 @@
 import pygame
 import sys
 import os
+import time
+import random
 from os import *
 pygame.init()
 pygame.font.init()
 pygame.font.get_fonts()
 
+virus_names = ["spyware", "ransomware", "malware"]
+
+screen = pygame.display.set_mode((1280, 720))
+
+level1Image = pygame.image.load("level_1.png")
+
+#game class
 class game:
     def __init__(self):
         self.width = 1280
@@ -14,12 +23,36 @@ class game:
         self.viruses = []
         self.anti_viruses = []
         self.money = 100
-        self.background = pygame.image.load(os.path.join("Game", "level_1.png"))
+        self.background = pygame.image.load("level_1.png")
+        #self.clicks = [] #remove later
+
+
+    def run(self):
+        run = True
+        fps = pygame.time.Clock()
+        while run:
+            fps.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                #pos = pygame.mouse.get_pos()
+
+                #if event.type == pygame.MOUSEBUTTONDOWN:
+                    #print(pos)    
+
+            self.draw()        
+
+    def draw(self):
+        self.screen.blit(self.background, (0,0))
+        #for p in self.clicks:
+            #pygame.draw.circle(self.screen, (255,0,0), (p[0], p[1]), 5)
+        pygame.display.update()
         
 
+#base virus class
 class virus: 
     images = []
-
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -42,39 +75,38 @@ class virus:
                 return True
         return False
 
-    def move(self):
+    #def move(self):
 
     def hit(self):
         self.health -=1
         if self.health <=0:
             return True
-    
+
+    def run(self):
+        run = True
+        fps = pygame.time.Clock()
+        while run:
+            fps.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.clicks.append(pos)
+                    print(pos)
+
+            self.draw()        
 
 
+        pygame.quit()            
 
+    def draw(self):
+        self.screen.blit(self.background, (0,0))
+        #for p in self.clicks:
+            #pygame.draw.circle(self.win, (255,0,0), (p[1], 5, 0))
+        pygame.display.update()
 
-#setting screen dimensions
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
-#loading in the assets
-level1 = pygame.image.load('level_1.png')
-level1Path = r'C:\Users\Harry\Documents\Computer science\Final year project\Assets\Game'
-
-#drawing window
-def draw():
-    screen.fill([0,0,0])
-    screen.blit(level1,[0,0])
-
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        
-    draw()
-    pygame.display.update()
-
+g = game()
+g.run()
